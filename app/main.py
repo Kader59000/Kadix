23d8322f6f04c3ae81fc44cc929e599de12f2ae8
amaraa <abdelkader.amara.etu@univ-lille.fr>
2025-11-26 13:47:28 +0100

    echo command

--- app/main.py
+++ app/main.py
@@ -4,11 +4,30 @@ import sys
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if (command == "exit"):
+        user_input = input()
+        splitted_input = user_input.split(" ")
+        command_input = splitted_input[0]
+        args = splitted_input[1:]
+        if (command_input == "exit"):
             break
-        print(f"{command}: command not found")
+        command = command_getter(command_input)
+        if (command != None):
+            command(args)
+            continue
+        else:
+            print(f"{command_input}: command not found")
     pass
+
+def echo_command(args):
+    for i in range (len(args) - 1):
+        print(args[i] + " ", end="")
+    print(args[-1], end="")
+    print("", end="\n")
+
+def command_getter(command):
+    if (command == "echo"):
+        return echo_command
+    return None
 
 
 if __name__ == "__main__":
