e8ad8c82ddb157c64ce9268f695505f6536a6a8c
amaraa <abdelkader.amara.etu@univ-lille.fr>
2025-12-01 17:51:14 +0100

    autocompletion

--- app/autocompletion/autocompleter.py
+++ app/autocompletion/autocompleter.py
@@ -5,6 +5,8 @@ class AutoCompleter:
     """
     Classe pour gérer l'autocomplétion des commandes internes et installées du shell.
     """
+    last_completed_command = ""
+
     def __init__(self):
         # Récupère la liste des commandes internes
         self.builtin_commands = list(BuiltinCommand.BUILTIN_COMMANDS.keys())
@@ -15,6 +17,11 @@ class AutoCompleter:
     def completer(self, text, state):
         # Filtre les commandes qui commencent par le texte saisi
         matches = [cmd for cmd in self.commands if cmd.startswith(text)]
+        if len(matches) > 1:
+            if state == 0:
+                return '\x07'  # Émet le bip (bell) sur le premier Tab si aucune suggestion
+            if state == 1:
+                return ' '.join(matches)
         if state < len(matches):
             return matches[state] + " "  # Ajoute un espace après la commande complétée
         return None
