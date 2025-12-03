fc484af67e08f5176801f10ebf523479d701262a
amaraa <abdelkader.amara.etu@univ-lille.fr>
2025-12-03 17:52:11 +0100

    pipeline builtin

--- app/commands/command.py
+++ app/commands/command.py
@@ -34,9 +34,11 @@ class InstalledCommand(Command):
         self.path = path
         self.args = args
  
-    def execute(self, stdin=None, stdout=None, stderr=None):
+    def execute(self, stdin=None, stdout=None, stderr=None, wait=True):
         """Lance la commande puis attend sa fin."""
         process = self.spawn(stdin=stdin, stdout=stdout, stderr=stderr)
+        if wait:
+            process.wait()
         sys.stdin = sys.__stdin__
         sys.stdout = sys.__stdout__
         sys.stderr = sys.__stderr__
