7ff12118aa35371425a25a5eeb897d345c80460b
amaraa <abdelkader.amara.etu@univ-lille.fr>
2025-12-03 10:59:50 +0100

    autocompletion lcp

--- app/autocompletion/autocompleter.py
+++ app/autocompletion/autocompleter.py
@@ -36,7 +36,9 @@ class AutoCompleter:
     def completer(self, text, state):
         matches = [cmd for cmd in self.commands if cmd.startswith(text)]
         if len(matches) == 0: # if no matches, we ring the bell
-            return 'x07'  
+            return 'x07' 
+        if len(matches) == 1:
+            return matches[0] + " "
         lcp = self.longest_common_prefix(matches)
         if state == 0:
             return lcp 
